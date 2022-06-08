95881b1e32f096a9aae816380f0574b1e4e54463
NIKO <nzubovskiy@gmail.com>
2022-06-08 18:16:42 +0400

    First commit

---
+++ src/main/java/pro/sky/course2/hwenv6/HwEnv6Application.java
@@ -0,0 +1,13 @@
+package pro.sky.course2.hwenv6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwEnv6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwEnv6Application.class, args);
+    }
+
+}
